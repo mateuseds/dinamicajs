06df263efdf28193c876b03a3c432a5c532cdf5b
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2022-05-15 17:05:35 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -15,3 +15,30 @@ $(document).ready(function() {
         content: '<div class="openpopover"><img class="openpopover__img" src="assets/historia.jpeg" alt=""><p class="openpopover__title">100 Anos de História</p><p>O imposto atual foi instituído em 1922 por Artur Bernardes, com a proposta de financiar a saúde, educação e o desenvolvimento urbano, com taxas variando entre 8 e 20%, com as maiores sendo pagas pelos de remuneração mais alta.</p></div>'
     });
 });
+
+$(document).ready(function() {
+    $('.terceirainfo').popover({
+        placement: 'bottom',
+        trigger: 'hover',
+        html: true,
+        content: '<div class="openpopover"><img class="openpopover__img" src="assets/loa.jpg" alt=""><p class="openpopover__title">Lei Orçamentária</p><p>A lei orçamentária anual (LOA) é a planilha do orçamento estatal com a estimativa de receitas e fixação de despesas executadas ao longo do ano. De forma muito detalhada, o Estado apresenta o planejamento de pagamento com pessoal, aposentadoria e dos investimentos nas áreas de governo, como saúde e segurança.</p></div>'
+    });
+});
+
+$(document).ready(function() {
+    $('.quartainfo').popover({
+        placement: 'right',
+        trigger: 'hover',
+        html: true,
+        content: '<div class="openpopover"><img class="openpopover__img" src="assets/tabela.png" alt=""><p class="openpopover__title">Imposto de Renda Retido na Fonte</p></div>'
+    });
+});
+
+$(document).ready(function() {
+    $('.quintainfo').popover({
+        placement: 'top',
+        trigger: 'hover',
+        html: true,
+        content: '<div class="openpopover"><img class="openpopover__img" src="assets/pucminas.jpg" alt=""><p>Pontifícia Universidade Católica de Minas Gerais</p></div>'
+    });
+});
