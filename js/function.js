d6dc6ba99f17e2bd7717029d1ed78aaae16f080e
Mateus Souza <80061497+mateuseds@users.noreply.github.com>
2022-05-16 17:18:09 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,33 +1,34 @@
+const popoverOpts = {
+    placement: 'bottom',
+    trigger: 'hover',
+    html: true,
+}
+
+const popoverContent = '<div class="openpopover">';
+
+
 $(document).ready(function() {
     $('.primeirainfo').popover({
-        placement: 'bottom',
-        trigger: 'hover',
-        html: true,
-        content: '<div class="openpopover"><img class="openpopover__img" src="assets/imposto.jpg" alt=""><p class="openpopover__title">Imposto de Renda</p><p>Também conhecido atualmente como o leão, se trata de um tributo federal sobre a renda (o quanto você ganha) e acompanha a sua evolução patrimonial. Todos os anos o Governo exige que trabalhadores do Brasil inteiro façam a declaração dos ganhos anuais para a Receita Federal.</p></div>'
+        ...popoverOpts,
+        content: `${popoverContent}<img class="openpopover__img" src="assets/imposto.jpg" alt=""><p class="openpopover__title">Imposto de Renda</p><p>Também conhecido atualmente como o leão, se trata de um tributo federal sobre a renda (o quanto você ganha) e acompanha a sua evolução patrimonial. Todos os anos o Governo exige que trabalhadores do Brasil inteiro façam a declaração dos ganhos anuais para a Receita Federal.</p></div>`
     });
     $('.segundainfo').popover({
-        placement: 'bottom',
-        trigger: 'hover',
-        html: true,
-        content: '<div class="openpopover"><img class="openpopover__img" src="assets/historia.jpeg" alt=""><p class="openpopover__title">100 Anos de História</p><p>O imposto atual foi instituído em 1922 por Artur Bernardes, com a proposta de financiar a saúde, educação e o desenvolvimento urbano, com taxas variando entre 8 e 20%, com as maiores sendo pagas pelos de remuneração mais alta.</p></div>'
+        ...popoverOpts,
+        content: `${popoverContent}<img class="openpopover__img" src="assets/historia.jpeg" alt=""><p class="openpopover__title">100 Anos de História</p><p>O imposto atual foi instituído em 1922 por Artur Bernardes, com a proposta de financiar a saúde, educação e o desenvolvimento urbano, com taxas variando entre 8 e 20%, com as maiores sendo pagas pelos de remuneração mais alta.</p></div>`
     });
     $('.terceirainfo').popover({
-        placement: 'bottom',
-        trigger: 'hover',
-        html: true,
-        content: '<div class="openpopover"><img class="openpopover__img" src="assets/loa.jpg" alt=""><p class="openpopover__title">Lei Orçamentária</p><p>A lei orçamentária anual (LOA) é a planilha do orçamento estatal com a estimativa de receitas e fixação de despesas executadas ao longo do ano. De forma muito detalhada, o Estado apresenta o planejamento de pagamento com pessoal, aposentadoria e dos investimentos nas áreas de governo, como saúde e segurança.</p></div>'
+        ...popoverOpts,
+        content: `${popoverContent}<img class="openpopover__img" src="assets/loa.jpg" alt=""><p class="openpopover__title">Lei Orçamentária</p><p>A lei orçamentária anual (LOA) é a planilha do orçamento estatal com a estimativa de receitas e fixação de despesas executadas ao longo do ano. De forma muito detalhada, o Estado apresenta o planejamento de pagamento com pessoal, aposentadoria e dos investimentos nas áreas de governo, como saúde e segurança.</p></div>`
     });
     $('.quartainfo').popover({
+        ...popoverOpts,
         placement: 'right',
-        trigger: 'hover',
-        html: true,
-        content: '<div class="openpopover"><img class="openpopover__img" src="assets/tabela.png" alt=""><p class="openpopover__title">Imposto de Renda Retido na Fonte</p></div>'
+        content: `${popoverContent}<img class="openpopover__img" src="assets/tabela.png" alt=""><p class="openpopover__title">Imposto de Renda Retido na Fonte</p></div>`
     });
     $('.quintainfo').popover({
+        ...popoverOpts,
         placement: 'top',
-        trigger: 'hover',
-        html: true,
-        content: '<div class="openpopover"><img class="openpopover__img" src="assets/pucminas.jpg" alt=""><p class="openpopover__p">Pontifícia Universidade Católica de Minas Gerais</p></div>'
+        content: `${popoverContent}<img class="openpopover__img" src="assets/pucminas.jpg" alt=""><p class="openpopover__p">Pontifícia Universidade Católica de Minas Gerais</p></div>`
     });
     dinamicaModal();
     dinamicaReferencias();
@@ -51,15 +52,13 @@ function dinamicaModal() {
     openModalElement.addEventListener("mouseover", mouseOver);
 };
 
-
-
 function dinamicaReferencias() {
-    const coll = document.getElementsByClassName("dinamicaCollapsedButton");
+    const coll = document.getElementsByClassName("collapsedButton");
     let i;
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
+            this.classList.toggle("collapsedButton--active");
             const conteudo = this.nextElementSibling;
             if (conteudo.style.display === "block") {
                 conteudo.style.display = "none";
